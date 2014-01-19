2685a5f181c9c036d7ed8d60c0b1d52126ad20ff
mguizani <manel.guizani@tuniteam.com>
2014-01-20 00:30:47 +0100

    commit line 4

--- TestBranch/src/com/tuniteam/occygen/TestMainClass.java
+++ TestBranch/src/com/tuniteam/occygen/TestMainClass.java
@@ -5,5 +5,6 @@ public class TestMainClass {
 String chaine = "line 1 to commit";
 String chaine2 = "line 2 to commit";
 String chaine3 = "line 3 to commit";
+String chaine4 = "line 4 to commit";
 
 }
