cafdedcafbb6e106bb318e64c8347da3a830db46
mguizani <manel.guizani@tuniteam.com>
2014-01-20 18:32:35 +0100

    bb

--- TestBranch/src/com/tuniteam/occygen/TestMainClass.java
+++ TestBranch/src/com/tuniteam/occygen/TestMainClass.java
@@ -14,6 +14,7 @@ String chaine11 = "line11 to commit for gerrit";
 String chaine12 = "line12 to commit for gerrit";
 String chaine119 = "line19 to commit for gerrit";
 String a1 = "a to gerrit ";
+String b = "a to gerrit ";
 
 
 }
