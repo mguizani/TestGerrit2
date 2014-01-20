4671738cc82aa925258609f0ec29442afe06a73d
mguizani <manel.guizani@tuniteam.com>
2014-01-20 19:47:27 +0100

    ch 2 2

--- TestBranch/src/com/tuniteam/occygen/TestMainClass.java
+++ TestBranch/src/com/tuniteam/occygen/TestMainClass.java
@@ -2,27 +2,7 @@ package com.tuniteam.occygen;
 
 public class TestMainClass {
 	
-String chaine = "line 1 to commit";
-String chaine2 = "line 2 to commit";
-String chaine3 = "line 3 to commit";
-String chaine4 = "line 4 to commit";
-String chaine5 = "line 5 to commit";
-String chaine6 = "line 6 to commit";
-String chaine7 = "line 7 to commit for gerrit";
 
-String chaine11 = "line11 to commit for gerrit";
-String chaine12 = "line12 to commit for gerrit";
-String chaine119 = "line19 to commit for gerrit";
-String a1 = "a to gerrit ";
-String b = "a to gerrit ";
-String cc = "a to gerrit ";
-String dd = "a to gerrit ";
-String ee = "a to gerrit ";
-String ff = "a to gerrit ";
-String jj = "a to gerrit branch 2 ";
-String kk = "a to gerrit branch 2 ";
-String bb = "a to gerrit branch 2 ";
-String ch1 = "ch1 to gerrit branch 1 ";
 String ch2 = "ch2 to gerrit branch 1 ";
 
 
